881dd16336c6520b20c4f1085e31df020c1c7d27
azy64 <allysaidi64@gmail.com>
2022-01-19 10:08:28 +0100

    Setup the project

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/users/:id(.:format)', to: 'users#show', as: 'user'
+  get '/users/(.:format)', to: 'users#index', as: 'users'
+  get '/users/:user_id/posts(.:format)', to: 'posts#index', as: 'user_posts'
+  get '/users/:user_id/posts/:id(.:format)', to: 'posts#show', as: 'user_post'
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
