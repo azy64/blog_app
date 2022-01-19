47a602973e0010373c5afd32b87c87e6854d34fe
azy64 <allysaidi64@gmail.com>
2022-01-19 21:57:57 +0100

    add test for controllers

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
+    it 'should return 200 response' do
+      get users_index_path
+      expect(response).to have_http_status(200)
+    end
+    it 'should render index template correctly template ' do
+      get users_index_path
+      expect(response).to render_template(:index)
+    end
+
+    it '' do
+      get users_index_path
+      expect(response.body).to include('USERS')
+    end
+  end
+  describe 'GET #show' do
+    before(:each) { get users_index_path(12) }
+
+    it 'should return 200 response' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render the show template ' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should include the word Single User' do
+      expect(response.body).to include('Single User')
+    end
+  end
+end
